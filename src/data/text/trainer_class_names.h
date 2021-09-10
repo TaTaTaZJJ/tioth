6c8b3cbfc0c9e75b70f96fb550b9f5b5b53a7fc1
TaTaTaZJJ <2519784590@qq.com>
2021-09-11 00:48:26 +0800

    汉化：训练师类型

--- src/data/text/trainer_class_names.h
+++ src/data/text/trainer_class_names.h
@@ -1,139 +1,465 @@
 #include "config.h"
 
 const u8 gTrainerClassNames[][13] = {
-#ifdef CHINESE //TODO 汉化
-    [TRAINER_CLASS_PKMN_TRAINER_1] = _("{PKMN} TRAINER"),
-    [TRAINER_CLASS_PKMN_TRAINER_2] = _("{PKMN} TRAINER"),
-    [TRAINER_CLASS_HIKER] = _("HIKER"),
-    [TRAINER_CLASS_TEAM_AQUA] = _("TEAM AQUA"),
-    [TRAINER_CLASS_PKMN_BREEDER] = _("{PKMN} BREEDER"),
-    [TRAINER_CLASS_COOLTRAINER] = _("COOLTRAINER"),
-    [TRAINER_CLASS_BIRD_KEEPER] = _("BIRD KEEPER"),
-    [TRAINER_CLASS_COLLECTOR] = _("COLLECTOR"),
-    [TRAINER_CLASS_SWIMMER_M] = _("SWIMMER♂"),
-    [TRAINER_CLASS_TEAM_MAGMA] = _("TEAM MAGMA"),
-    [TRAINER_CLASS_EXPERT] = _("EXPERT"),
-    [TRAINER_CLASS_AQUA_ADMIN] = _("AQUA ADMIN"),
-    [TRAINER_CLASS_BLACK_BELT] = _("BLACK BELT"),
-    [TRAINER_CLASS_AQUA_LEADER] = _("AQUA LEADER"),
-    [TRAINER_CLASS_HEX_MANIAC] = _("HEX MANIAC"),
-    [TRAINER_CLASS_AROMA_LADY] = _("AROMA LADY"),
-    [TRAINER_CLASS_RUIN_MANIAC] = _("RUIN MANIAC"),
-    [TRAINER_CLASS_INTERVIEWER] = _("INTERVIEWER"),
-    [TRAINER_CLASS_TUBER_F] = _("TUBER"),
-    [TRAINER_CLASS_TUBER_M] = _("TUBER"),
-    [TRAINER_CLASS_LADY] = _("LADY"),
-    [TRAINER_CLASS_BEAUTY] = _("BEAUTY"),
-    [TRAINER_CLASS_RICH_BOY] = _("RICH BOY"),
-    [TRAINER_CLASS_POKEMANIAC] = _("POKéMANIAC"),
-    [TRAINER_CLASS_GUITARIST] = _("GUITARIST"),
-    [TRAINER_CLASS_KINDLER] = _("KINDLER"),
-    [TRAINER_CLASS_CAMPER] = _("CAMPER"),
-    [TRAINER_CLASS_PICNICKER] = _("PICNICKER"),
-    [TRAINER_CLASS_BUG_MANIAC] = _("BUG MANIAC"),
-    [TRAINER_CLASS_PSYCHIC] = _("PSYCHIC"),
-    [TRAINER_CLASS_GENTLEMAN] = _("GENTLEMAN"),
-    [TRAINER_CLASS_ELITE_FOUR] = _("ELITE FOUR"),
-    [TRAINER_CLASS_LEADER] = _("LEADER"),
-    [TRAINER_CLASS_SCHOOL_KID] = _("SCHOOL KID"),
-    [TRAINER_CLASS_SR_AND_JR] = _("SR. AND JR."),
-    [TRAINER_CLASS_WINSTRATE] = _("WINSTRATE"),
-    [TRAINER_CLASS_POKEFAN] = _("POKéFAN"),
-    [TRAINER_CLASS_YOUNGSTER] = _("YOUNGSTER"),
-    [TRAINER_CLASS_CHAMPION] = _("CHAMPION"),
-    [TRAINER_CLASS_FISHERMAN] = _("FISHERMAN"),
-    [TRAINER_CLASS_TRIATHLETE] = _("TRIATHLETE"),
-    [TRAINER_CLASS_DRAGON_TAMER] = _("DRAGON TAMER"),
-    [TRAINER_CLASS_NINJA_BOY] = _("NINJA BOY"),
-    [TRAINER_CLASS_BATTLE_GIRL] = _("BATTLE GIRL"),
-    [TRAINER_CLASS_PARASOL_LADY] = _("PARASOL LADY"),
-    [TRAINER_CLASS_SWIMMER_F] = _("SWIMMER♀"),
-    [TRAINER_CLASS_TWINS] = _("TWINS"),
-    [TRAINER_CLASS_SAILOR] = _("SAILOR"),
-    [TRAINER_CLASS_COOLTRAINER_2] = _("COOLTRAINER"),
-    [TRAINER_CLASS_MAGMA_ADMIN] = _("MAGMA ADMIN"),
-    [TRAINER_CLASS_PKMN_TRAINER_3] = _("{PKMN} TRAINER"),
-    [TRAINER_CLASS_BUG_CATCHER] = _("BUG CATCHER"),
-    [TRAINER_CLASS_PKMN_RANGER] = _("{PKMN} RANGER"),
-    [TRAINER_CLASS_MAGMA_LEADER] = _("MAGMA LEADER"),
-    [TRAINER_CLASS_LASS] = _("LASS"),
-    [TRAINER_CLASS_YOUNG_COUPLE] = _("YOUNG COUPLE"),
-    [TRAINER_CLASS_OLD_COUPLE] = _("OLD COUPLE"),
-    [TRAINER_CLASS_SIS_AND_BRO] = _("SIS AND BRO"),
-    [TRAINER_CLASS_SALON_MAIDEN] = _("SALON MAIDEN"),
-    [TRAINER_CLASS_DOME_ACE] = _("DOME ACE"),
-    [TRAINER_CLASS_PALACE_MAVEN] = _("PALACE MAVEN"),
-    [TRAINER_CLASS_ARENA_TYCOON] = _("ARENA TYCOON"),
-    [TRAINER_CLASS_FACTORY_HEAD] = _("FACTORY HEAD"),
-    [TRAINER_CLASS_PIKE_QUEEN] = _("PIKE QUEEN"),
-    [TRAINER_CLASS_PYRAMID_KING] = _("PYRAMID KING"),
-    [TRAINER_CLASS_RS_PROTAG] = _("{PKMN} TRAINER"),
+    [TRAINER_CLASS_PKMN_TRAINER_1] =
+#ifdef CHINESE
+        _("{PKMN}训练家"),
 #else
-    [TRAINER_CLASS_PKMN_TRAINER_1] = _("{PKMN} TRAINER"),
-    [TRAINER_CLASS_PKMN_TRAINER_2] = _("{PKMN} TRAINER"),
-    [TRAINER_CLASS_HIKER] = _("HIKER"),
-    [TRAINER_CLASS_TEAM_AQUA] = _("TEAM AQUA"),
-    [TRAINER_CLASS_PKMN_BREEDER] = _("{PKMN} BREEDER"),
-    [TRAINER_CLASS_COOLTRAINER] = _("COOLTRAINER"),
-    [TRAINER_CLASS_BIRD_KEEPER] = _("BIRD KEEPER"),
-    [TRAINER_CLASS_COLLECTOR] = _("COLLECTOR"),
-    [TRAINER_CLASS_SWIMMER_M] = _("SWIMMER♂"),
-    [TRAINER_CLASS_TEAM_MAGMA] = _("TEAM MAGMA"),
-    [TRAINER_CLASS_EXPERT] = _("EXPERT"),
-    [TRAINER_CLASS_AQUA_ADMIN] = _("AQUA ADMIN"),
-    [TRAINER_CLASS_BLACK_BELT] = _("BLACK BELT"),
-    [TRAINER_CLASS_AQUA_LEADER] = _("AQUA LEADER"),
-    [TRAINER_CLASS_HEX_MANIAC] = _("HEX MANIAC"),
-    [TRAINER_CLASS_AROMA_LADY] = _("AROMA LADY"),
-    [TRAINER_CLASS_RUIN_MANIAC] = _("RUIN MANIAC"),
-    [TRAINER_CLASS_INTERVIEWER] = _("INTERVIEWER"),
-    [TRAINER_CLASS_TUBER_F] = _("TUBER"),
-    [TRAINER_CLASS_TUBER_M] = _("TUBER"),
-    [TRAINER_CLASS_LADY] = _("LADY"),
-    [TRAINER_CLASS_BEAUTY] = _("BEAUTY"),
-    [TRAINER_CLASS_RICH_BOY] = _("RICH BOY"),
-    [TRAINER_CLASS_POKEMANIAC] = _("POKéMANIAC"),
-    [TRAINER_CLASS_GUITARIST] = _("GUITARIST"),
-    [TRAINER_CLASS_KINDLER] = _("KINDLER"),
-    [TRAINER_CLASS_CAMPER] = _("CAMPER"),
-    [TRAINER_CLASS_PICNICKER] = _("PICNICKER"),
-    [TRAINER_CLASS_BUG_MANIAC] = _("BUG MANIAC"),
-    [TRAINER_CLASS_PSYCHIC] = _("PSYCHIC"),
-    [TRAINER_CLASS_GENTLEMAN] = _("GENTLEMAN"),
-    [TRAINER_CLASS_ELITE_FOUR] = _("ELITE FOUR"),
-    [TRAINER_CLASS_LEADER] = _("LEADER"),
-    [TRAINER_CLASS_SCHOOL_KID] = _("SCHOOL KID"),
-    [TRAINER_CLASS_SR_AND_JR] = _("SR. AND JR."),
-    [TRAINER_CLASS_WINSTRATE] = _("WINSTRATE"),
-    [TRAINER_CLASS_POKEFAN] = _("POKéFAN"),
-    [TRAINER_CLASS_YOUNGSTER] = _("YOUNGSTER"),
-    [TRAINER_CLASS_CHAMPION] = _("CHAMPION"),
-    [TRAINER_CLASS_FISHERMAN] = _("FISHERMAN"),
-    [TRAINER_CLASS_TRIATHLETE] = _("TRIATHLETE"),
-    [TRAINER_CLASS_DRAGON_TAMER] = _("DRAGON TAMER"),
-    [TRAINER_CLASS_NINJA_BOY] = _("NINJA BOY"),
-    [TRAINER_CLASS_BATTLE_GIRL] = _("BATTLE GIRL"),
-    [TRAINER_CLASS_PARASOL_LADY] = _("PARASOL LADY"),
-    [TRAINER_CLASS_SWIMMER_F] = _("SWIMMER♀"),
-    [TRAINER_CLASS_TWINS] = _("TWINS"),
-    [TRAINER_CLASS_SAILOR] = _("SAILOR"),
-    [TRAINER_CLASS_COOLTRAINER_2] = _("COOLTRAINER"),
-    [TRAINER_CLASS_MAGMA_ADMIN] = _("MAGMA ADMIN"),
-    [TRAINER_CLASS_PKMN_TRAINER_3] = _("{PKMN} TRAINER"),
-    [TRAINER_CLASS_BUG_CATCHER] = _("BUG CATCHER"),
-    [TRAINER_CLASS_PKMN_RANGER] = _("{PKMN} RANGER"),
-    [TRAINER_CLASS_MAGMA_LEADER] = _("MAGMA LEADER"),
-    [TRAINER_CLASS_LASS] = _("LASS"),
-    [TRAINER_CLASS_YOUNG_COUPLE] = _("YOUNG COUPLE"),
-    [TRAINER_CLASS_OLD_COUPLE] = _("OLD COUPLE"),
-    [TRAINER_CLASS_SIS_AND_BRO] = _("SIS AND BRO"),
-    [TRAINER_CLASS_SALON_MAIDEN] = _("SALON MAIDEN"),
-    [TRAINER_CLASS_DOME_ACE] = _("DOME ACE"),
-    [TRAINER_CLASS_PALACE_MAVEN] = _("PALACE MAVEN"),
-    [TRAINER_CLASS_ARENA_TYCOON] = _("ARENA TYCOON"),
-    [TRAINER_CLASS_FACTORY_HEAD] = _("FACTORY HEAD"),
-    [TRAINER_CLASS_PIKE_QUEEN] = _("PIKE QUEEN"),
-    [TRAINER_CLASS_PYRAMID_KING] = _("PYRAMID KING"),
-    [TRAINER_CLASS_RS_PROTAG] = _("{PKMN} TRAINER"),
+        _("{PKMN} TRAINER"),
+#endif
+
+    [TRAINER_CLASS_PKMN_TRAINER_2] =
+#ifdef CHINESE
+        _("{PKMN}训练家"),
+#else
+        _("{PKMN} TRAINER"),
+#endif
+
+    [TRAINER_CLASS_HIKER] =
+#ifdef CHINESE
+        _("登山男"),
+#else
+        _("HIKER"),
+#endif
+
+    [TRAINER_CLASS_TEAM_AQUA] =
+#ifdef CHINESE
+        _("海洋队"),
+#else
+        _("TEAM AQUA"),
+#endif
+
+    [TRAINER_CLASS_PKMN_BREEDER] =
+#ifdef CHINESE
+        _("{PKMN}培育家"),
+#else
+        _("{PKMN} BREEDER"),
+#endif
+
+    [TRAINER_CLASS_COOLTRAINER] =
+#ifdef CHINESE
+        _("精英训练家"),
+#else
+        _("COOLTRAINER"),
+#endif
+
+    [TRAINER_CLASS_BIRD_KEEPER] =
+#ifdef CHINESE
+        _("养鸟人"),
+#else
+        _("BIRD KEEPER"),
+#endif
+
+    [TRAINER_CLASS_COLLECTOR] =
+#ifdef CHINESE
+        _("宝可梦收藏家"),
+#else
+        _("COLLECTOR"),
+#endif
+
+    [TRAINER_CLASS_SWIMMER_M] =
+#ifdef CHINESE
+        _("泳裤小伙子"),
+#else
+        _("SWIMMER♂"),
+#endif
+
+    [TRAINER_CLASS_TEAM_MAGMA] =
+#ifdef CHINESE
+        _("熔岩队"),
+#else
+        _("TEAM MAGMA"),
+#endif
+
+    [TRAINER_CLASS_EXPERT] =
+#ifdef CHINESE
+        _("达人"),
+#else
+        _("EXPERT"),
+#endif
+
+    [TRAINER_CLASS_AQUA_ADMIN] =
+#ifdef CHINESE
+        _("海洋队干部"),
+#else
+        _("AQUA ADMIN"),
+#endif
+
+    [TRAINER_CLASS_BLACK_BELT] =
+#ifdef CHINESE
+        _("空手道王"),
+#else
+        _("BLACK BELT"),
+#endif
+
+    [TRAINER_CLASS_AQUA_LEADER] =
+#ifdef CHINESE
+        _("水梧桐"),
+#else
+        _("AQUA LEADER"),
+#endif
+
+    [TRAINER_CLASS_HEX_MANIAC] =
+#ifdef CHINESE
+        _("灵异迷"),
+#else
+        _("HEX MANIAC"),
+#endif
+
+    [TRAINER_CLASS_AROMA_LADY] =
+#ifdef CHINESE
+        _("香氛姐姐"),
+#else
+        _("AROMA LADY"),
+#endif
+
+    [TRAINER_CLASS_RUIN_MANIAC] =
+#ifdef CHINESE
+        _("遗迹迷"),
+#else
+        _("RUIN MANIAC"),
+#endif
+
+    [TRAINER_CLASS_INTERVIEWER] =
+#ifdef CHINESE
+        _("采访者"),
+#else
+        _("INTERVIEWER"),
+#endif
+
+    [TRAINER_CLASS_TUBER_F] =
+#ifdef CHINESE
+        _("泳圈男孩"),
+#else
+        _("TUBER"),
+#endif
+
+    [TRAINER_CLASS_TUBER_M] =
+#ifdef CHINESE
+        _("泳圈女孩"),
+#else
+        _("TUBER"),
+#endif
+
+    [TRAINER_CLASS_LADY] =
+#ifdef CHINESE
+        _("千金小姐"),
+#else
+        _("LADY"),
+#endif
+
+    [TRAINER_CLASS_BEAUTY] =
+#ifdef CHINESE
+        _("大姐姐"),
+#else
+        _("BEAUTY"),
+#endif
+
+    [TRAINER_CLASS_RICH_BOY] =
+#ifdef CHINESE
+        _("富家少爷"),
+#else
+        _("RICH BOY"),
+#endif
+
+    [TRAINER_CLASS_POKEMANIAC] =
+#ifdef CHINESE
+        _("怪兽狂"),
+#else
+        _("POKéMANIAC"),
+#endif
+
+    [TRAINER_CLASS_GUITARIST] =
+#ifdef CHINESE
+        _("吉他手"),
+#else
+        _("GUITARIST"),
+#endif
+
+    [TRAINER_CLASS_KINDLER] =
+#ifdef CHINESE
+        _("营火专家"),
+#else
+        _("KINDLER"),
+#endif
+
+    [TRAINER_CLASS_CAMPER] =
+#ifdef CHINESE
+        _("露营少年"),
+#else
+        _("CAMPER"),
+#endif
+
+    [TRAINER_CLASS_PICNICKER] =
+#ifdef CHINESE
+        _("野餐女孩"),
+#else
+        _("PICNICKER"),
+#endif
+
+    [TRAINER_CLASS_BUG_MANIAC] =
+#ifdef CHINESE
+        _("昆虫狂"),
+#else
+        _("BUG MANIAC"),
+#endif
+
+    [TRAINER_CLASS_PSYCHIC] =
+#ifdef CHINESE
+        _("超能力者"),
+#else
+        _("PSYCHIC"),
+#endif
+
+    [TRAINER_CLASS_GENTLEMAN] =
+#ifdef CHINESE
+        _("绅士"),
+#else
+        _("GENTLEMAN"),
+#endif
+
+    [TRAINER_CLASS_ELITE_FOUR] =
+#ifdef CHINESE
+        _("四天王"),
+#else
+        _("ELITE FOUR"),
+#endif
+
+    [TRAINER_CLASS_LEADER] =
+#ifdef CHINESE
+        _("道馆馆主"),
+#else
+        _("LEADER"),
+#endif
+
+    [TRAINER_CLASS_SCHOOL_KID] =
+#ifdef CHINESE
+        _("小学生"),
+#else
+        _("SCHOOL KID"),
+#endif
+
+    [TRAINER_CLASS_SR_AND_JR] =
+#ifdef CHINESE
+        _("学姐与学妹"),
+#else
+        _("SR. AND JR."),
+#endif
+
+    [TRAINER_CLASS_WINSTRATE] =
+#ifdef CHINESE
+        _("连胜家族"),
+#else
+        _("WINSTRATE"),
+#endif
+
+    [TRAINER_CLASS_POKEFAN] =
+#ifdef CHINESE
+        _("同好夫妇"),
+#else
+        _("POKéFAN"),
+#endif
+
+    [TRAINER_CLASS_YOUNGSTER] =
+#ifdef CHINESE
+        _("短裤小子"),
+#else
+        _("YOUNGSTER"),
+#endif
+
+    [TRAINER_CLASS_CHAMPION] =
+#ifdef CHINESE
+        _("冠军"),
+#else
+        _("CHAMPION"),
+#endif
+
+    [TRAINER_CLASS_FISHERMAN] =
+#ifdef CHINESE
+        _("垂钓者"),
+#else
+        _("FISHERMAN"),
+#endif
+
+    [TRAINER_CLASS_TRIATHLETE] =
+#ifdef CHINESE
+        _("铁人三项"),
+#else
+        _("TRIATHLETE"),
+#endif
+
+    [TRAINER_CLASS_DRAGON_TAMER] =
+#ifdef CHINESE
+        _("驯龙师"),
+#else
+        _("DRAGON TAMER"),
+#endif
+
+    [TRAINER_CLASS_NINJA_BOY] =
+#ifdef CHINESE
+        _("忍者小子"),
+#else
+        _("NINJA BOY"),
+#endif
+
+    [TRAINER_CLASS_BATTLE_GIRL] =
+#ifdef CHINESE
+        _("对战女孩"),
+#else
+        _("BATTLE GIRL"),
+#endif
+
+    [TRAINER_CLASS_PARASOL_LADY] =
+#ifdef CHINESE
+        _("阳伞姐姐"),
+#else
+        _("PARASOL LADY"),
+#endif
+
+    [TRAINER_CLASS_SWIMMER_F] =
+#ifdef CHINESE
+        _("比基尼姐姐"),
+#else
+        _("SWIMMER♀"),
+#endif
+
+    [TRAINER_CLASS_TWINS] =
+#ifdef CHINESE
+        _("双胞胎"),
+#else
+        _("TWINS"),
+#endif
+
+    [TRAINER_CLASS_SAILOR] =
+#ifdef CHINESE
+        _("水手"),
+#else
+        _("SAILOR"),
+#endif
+
+    [TRAINER_CLASS_COOLTRAINER_2] =
+#ifdef CHINESE
+        _("精英训练家"),
+#else
+        _("COOLTRAINER"),
+#endif
+
+    [TRAINER_CLASS_MAGMA_ADMIN] =
+#ifdef CHINESE
+        _("熔岩队干部"),
+#else
+        _("MAGMA ADMIN"),
+#endif
+
+    [TRAINER_CLASS_PKMN_TRAINER_3] =
+#ifdef CHINESE
+        _("{PKMN}训练家"),
+#else
+        _("{PKMN} TRAINER"),
+#endif
+
+    [TRAINER_CLASS_BUG_CATCHER] =
+#ifdef CHINESE
+        _("捕虫少年"),
+#else
+        _("BUG CATCHER"),
+#endif
+
+    [TRAINER_CLASS_PKMN_RANGER] =
+#ifdef CHINESE
+        _("{PKMN}巡护员"),
+#else
+        _("{PKMN} RANGER"),
+#endif
+
+    [TRAINER_CLASS_MAGMA_LEADER] =
+#ifdef CHINESE
+        _("赤焰松"),
+#else
+        _("MAGMA LEADER"),
+#endif
+
+    [TRAINER_CLASS_LASS] =
+#ifdef CHINESE
+        _("迷你裙"),
+#else
+        _("LASS"),
+#endif
+
+    [TRAINER_CLASS_YOUNG_COUPLE] =
+#ifdef CHINESE
+        _("热恋情侣"),
+#else
+        _("YOUNG COUPLE"),
+#endif
+
+    [TRAINER_CLASS_OLD_COUPLE] =
+#ifdef CHINESE
+        _("金婚夫妻"),
+#else
+        _("OLD COUPLE"),
+#endif
+
+    [TRAINER_CLASS_SIS_AND_BRO] =
+#ifdef CHINESE
+        _("海上姐弟"),
+#else
+        _("SIS AND BRO"),
+#endif
+
+    [TRAINER_CLASS_SALON_MAIDEN] =
+#ifdef CHINESE
+        _("对战塔大君"),
+#else
+        _("SALON MAIDEN"),
+#endif
+
+    [TRAINER_CLASS_DOME_ACE] =
+#ifdef CHINESE
+        _("希尔斯"),
+#else
+        _("DOME ACE"),
+#endif
+
+    [TRAINER_CLASS_PALACE_MAVEN] =
+#ifdef CHINESE
+        _("宇康"),
+#else
+        _("PALACE MAVEN"),
+#endif
+
+    [TRAINER_CLASS_ARENA_TYCOON] =
+#ifdef CHINESE
+        _("黄瓜香"),
+#else
+        _("ARENA TYCOON"),
+#endif
+
+    [TRAINER_CLASS_FACTORY_HEAD] =
+#ifdef CHINESE
+        _("工厂头目"),
+#else
+        _("FACTORY HEAD"),
+#endif
+
+    [TRAINER_CLASS_PIKE_QUEEN] =
+#ifdef CHINESE
+        _("小蓟"),
+#else
+        _("PIKE QUEEN"),
+#endif
+
+    [TRAINER_CLASS_PYRAMID_KING] =
+#ifdef CHINESE
+        _("神代"),
+#else
+        _("PYRAMID KING"),
+#endif
+
+    [TRAINER_CLASS_RS_PROTAG] =
+#ifdef CHINESE
+        _("{PKMN}训练家"),
+#else
+        _("{PKMN} TRAINER"),
 #endif
 };
