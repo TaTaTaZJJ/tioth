5df57780908d530117a7a1aee77a05b754706c65
TaTaTaZJJ <2519784590@qq.com>
2021-09-11 23:46:07 +0800

    汉化：增加了全角字体码表 调整：道具最大文字数 调整：训练师类型字数

--- include/data.h
+++ include/data.h
@@ -121,7 +121,7 @@ extern const struct CompressedSpriteSheet gMonFrontPicTableFemale[];
 extern const bool8 SpeciesHasGenderDifference[NUM_SPECIES];
 
 extern const struct Trainer gTrainers[];
-extern const u8 gTrainerClassNames[][13];
+extern const u8 gTrainerClassNames[][19];
 extern const u8 gSpeciesNames[][POKEMON_NAME_LENGTH + 1];
 extern const u8 gMoveNames[MOVES_COUNT][MOVE_NAME_LENGTH + 1];
 
