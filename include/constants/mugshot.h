5db171cd97a544c9e5f818d3d623b9edab6e9a3b
Fora-Andy <56442308+Fora-Andy@users.noreply.github.com>
2021-10-03 22:43:12 +0800

    新增：Majo Normal和Waifu Normal头像

--- include/constants/mugshot.h
+++ include/constants/mugshot.h
@@ -1,12 +1,14 @@
 #ifndef GUARD_CONSTANTS_MUGSHOTS_H
 #define GUARD_CONSTANTS_MUGSHOTS_H
 
-#define MUGSHOT_START    0
+#define MUGSHOT_START           0
 
-#define MUGSHOT_TEST    0
-#define MUGSHOT_TEST2   1
-#define MUGSHOT_MAJO    2
-#define MUGSHOT_END     2
+#define MUGSHOT_TEST            0
+#define MUGSHOT_TEST2           1
+#define MUGSHOT_MAJO            2
+#define MUGSHOT_MAJO_NORMAl     3
+#define MUGSHOT_WAIFU_NORMAL    4
+#define MUGSHOT_END             4
 
 #define MUGSHOT_COUNT   (MUGSHOT_END - MUGSHOT_START + 1)
 
